201c069937fe8f085f803731c7052bb5e3e1b8d9
Sujit Majumdar <sujit.majumdar.personal@gmail.com>
2025-08-11 11:17:34 +0530

    Recursion

--- DS_ALGO/src/main/java/com/strivers/recursion/SumOfNumbers.java
+++ DS_ALGO/src/main/java/com/strivers/recursion/SumOfNumbers.java
@@ -1,5 +1,25 @@
 package com.strivers.recursion;
 
-public class SumOfNumbers {
+import org.junit.jupiter.api.Test;
 
+public class SumOfNumbers {
+    public int sum(int n,int sum){
+        if(n<1)
+            return sum;
+        sum+=n;
+        n--;
+        return sum(n,sum);
+    }
+
+    public int sum2(int n){
+        if(n==0)
+            return 0;
+        return n+sum2(n-1);
+    }
+
+    @Test
+    public void tc1(){
+        System.out.println(sum(5,0));
+        System.out.println(sum2(5));
+    }
 }
