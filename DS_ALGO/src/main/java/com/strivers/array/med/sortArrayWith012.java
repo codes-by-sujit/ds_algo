f7fd3db4f5dfc5424ec5ddaf5f4fa4846f429c7a
Sujit Majumdar <sujit.majumdar.personal@gmail.com>
2025-08-14 11:44:29 +0530

    Arrays

---
+++ DS_ALGO/src/main/java/com/strivers/array/med/sortArrayWith012.java
@@ -0,0 +1,35 @@
+package com.strivers.array.med;
+
+import com.strivers.util.ArrayUtil;
+import org.junit.jupiter.api.Test;
+
+public class sortArrayWith012 {
+    private void sort(int[] arr){
+        int low=0,mid=0,high=arr.length-1;
+        while(mid<high){
+            if(arr[mid]==0){
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            } else if(arr[mid]==1){
+                mid++;
+            } else {
+                swap(arr,high,mid);
+                high--;
+            }
+        }
+    }
+
+    private void swap(int[] arr, int i, int j){
+        int t=arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
+    }
+
+    @Test
+    public void tc(){
+        int[] arr = new int[]{0,0,1,2,2,1,0,1};
+        sort(arr);
+        ArrayUtil.print(arr);
+    }
+}
