d3062f6b3640333370868603d8955e18d2a4c516
sujitmajumdar42@gmail.com <sujitmajumdar42@gmail.com>
2025-09-19 16:11:27 +0530

    Attempt|LC:198|House Robber alternate approach

--- DS_ALGO/src/main/java/leetCode/dynamicprogramming/_198_HouseRobber.java
+++ DS_ALGO/src/main/java/leetCode/dynamicprogramming/_198_HouseRobber.java
@@ -17,15 +17,28 @@ public class _198_HouseRobber {
         return max;
     }
 
+    public int rob2(int[] nums) {
+        int size=nums.length;
+        int[] dp=new int[size];
+        if(size==1) return nums[0];
+        if(size==2) return Math.max(nums[0],nums[1]);
+        dp[0]=nums[0];
+        dp[1]=Math.max(dp[0],nums[1]);
+        for(int i = 2; i<size; i++){
+            dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return dp[dp.length-1];
+    }
+
     @Test
     public void tc1(){
         int[] nums=new int[]{2,7,9,3,1};
-        System.out.println(this.rob(nums));
+        System.out.println(this.rob2(nums));
     }
 
     @Test
     public void tc2(){
         int[] nums=new int[]{2,1,1,2};
-        System.out.println(this.rob(nums));
+        System.out.println(this.rob2(nums));
     }
 }
