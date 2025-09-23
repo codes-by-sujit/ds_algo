af2be016782c4249dcf80085971b630f811cf171
sujitmajumdar42@gmail.com <sujitmajumdar42@gmail.com>
2025-09-23 23:03:11 +0530

    Attempt | Striver | Convert postfix to infix

---
+++ DS_ALGO/src/main/java/com/strivers/stack/PostfixToInfix.java
@@ -0,0 +1,29 @@
+package com.strivers.stack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+public class PostfixToInfix {
+    private String convert(String expr){
+        Stack<String> stack=new Stack<>();
+        for(char c: expr.toCharArray()){
+            if(Character.isLetterOrDigit(c)) stack.push(c+"");
+            else{
+                String op2=stack.pop();
+                String op1=stack.pop();
+                stack.push("("+op1+c+op2+")");
+            }
+        }
+        return stack.pop();
+    }
+
+    @Test
+    public void tc1(){
+        String str1="pq+mn-*";
+        String str2="abcd^e-fgh*+^*+i-";
+        System.out.println(this.convert(str1));
+        System.out.println(this.convert(str2));
+    }
+
+}
